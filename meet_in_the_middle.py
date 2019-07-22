2461ef2d36249b34709625ace5f008dc214e2f52
simon <simon.crane@cambridgequantum.com>
2019-07-22 10:25:08 +0100

    implemented an exponential - time search for approximate circuits.  It is far too slow to be practical

--- meet_in_the_middle.py
+++ meet_in_the_middle.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import math
 from typing import Dict, Iterable
 
@@ -11,7 +12,7 @@ class VPTree:
             self.p = S[0]
             self.mu = 0
         else:
-            self.p = S[0]
+            self.p = random.choice(S)
             distances = sorted([(r, distance(self.p[1], r[1])) for r in S if r[0] != self.p[0]], key=lambda x: x[1])
             mu_index = (len(distances) - 1) // 2
             self.mu = distances[mu_index][1]
@@ -29,7 +30,7 @@ def distance(p, q):
 
 
 def nearest_neighbour(unitary: np.ndarray, tree: VPTree, precision: float):
-    # is there a member of the set with a distance precision of the given unitary?
+    # is there a member of the set within a distance precision of the given unitary?
     if tree is None:
         return False, None
     if tree.p is None:
@@ -46,9 +47,15 @@ def nearest_neighbour(unitary: np.ndarray, tree: VPTree, precision: float):
 def approx(gate_set: Dict[str, np.ndarray], unitary: np.ndarray, max_length: int, precision: float) -> Iterable:
     S = gate_set
     for i in range(math.ceil(max_length / 2)):
-        S = {k1 + k2: v1 @ v2 for k1, v1 in gate_set.items() for k2, v2 in S.items()}
+        print(i)
+        new_S = {k1 + k2: v1 @ v2 for k1, v1 in gate_set.items() for k2, v2 in S.items()}
+        tree = VPTree(list(S.items()))
         for k, v in S.items():
-            success, w = nearest_neighbour(v.transpose().conjugate() @ unitary, VPTree(list(S.items())), precision)
+            success, w = nearest_neighbour(v.transpose().conjugate() @ unitary, tree, precision)
             if success:
-                return k, v @ w[1]
-            # Not quite sure why the for loop doesn't take care of it, but she does the above again for S_(i+1)
+                return k + w[0], v @ w[1]
+        for k, v in new_S.items():
+            success, w = nearest_neighbour(v.transpose().conjugate() @ unitary, tree, precision)
+            if success:
+                return k + w[0], v @ w[1]
+        S = new_S
--- mitm.test.py
+++ mitm.test.py
@@ -1,5 +1,6 @@
 from meet_in_the_middle import *
 from Pauli import *
+from common_gates import Rz
 
 
 gate_set = {"S": np.array([[1, 0], [0, complex(0, 1)]]),
@@ -7,6 +8,13 @@ gate_set = {"S": np.array([[1, 0], [0, complex(0, 1)]]),
             "H": 0.5 ** 0.5 * np.array([[1, 1], [1, -1]]),
             "X": np.array([[0, 1], [1, 0]])}
 
+m = Rz(math.pi / 3, 0, 1)
 
-print(approx(gate_set, np.eye(2), 6, 0.5))
+print(m)
 
+for n in range(10):
+    a = approx(gate_set, m, 3 * n, 1 - n/10)
+    print(a)
+    if a is not None:
+        print(distance(a[1], m))
+    print()
