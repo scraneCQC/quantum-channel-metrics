6cfabd9bad1c11cb9166c42312ea7779b5ac6646
simon <simon.crane@cambridgequantum.com>
2019-07-26 11:09:59 +0100

    does replacement as well as deletion

--- remove_subcircuits.py
+++ remove_subcircuits.py
@@ -8,6 +8,7 @@ class SubcircuitRemover:
     def __init__(self, circuit_description, circuit_key, noise_channels, n_qubits=1):
         self.circuit = circuit_description
         self.key = circuit_key
+        self.gates = list(self.key.keys())
         self.noise_channels = noise_channels
         self.n_qubits = n_qubits
         self.original_unitary = self.unitary
@@ -19,7 +20,17 @@ class SubcircuitRemover:
         dont_do_it = f_pro([unitary], np.eye(unitary.shape[0]))
         return do_it <= dont_do_it
 
-    def test_all_subcircuits(self):
+    def should_replace_subcircuit(self, start, end):
+        subcircuit = self.circuit[start: end]
+        unitary = reduce(lambda x, y: x @ y, [self.key[s] for s in subcircuit], np.eye(2 ** self.n_qubits))
+        replace_fid = [(g, f_pro_experimental(g, unitary, self.noise_channels, self.key)) for g in self.gates]
+        original_fid = f_pro_experimental(subcircuit, unitary, self.noise_channels, self.key)
+        g, f = max(replace_fid, key=lambda x: x[1])
+        if f > original_fid:
+            return g
+        return False
+
+    def remove_any_subcircuit(self):
         length = len(self.circuit)
         for i in range(length, 0, -1):  # Test subcircuits of length i
             for s in range(length - i + 1):  # That start at s
@@ -30,8 +41,19 @@ class SubcircuitRemover:
                     return True
         return False
 
+    def replace_any_subcircuit(self):
+        length = len(self.circuit)
+        for i in range(length, 1, -1):  # Test subcircuits of length i
+            for s in range(length - i + 1):  # That start at s
+                e = s + i
+                res = self.should_replace_subcircuit(s, s + i)
+                if res:
+                    self.circuit = self.circuit[:s] + res + self.circuit[e:]
+                    return True
+        return False
+
     def reduce_circuit(self):
-        while self.test_all_subcircuits():
+        while self.replace_any_subcircuit() or self.remove_any_subcircuit():
             pass
 
     @property
--- remove_subcircuits.test.py
+++ remove_subcircuits.test.py
@@ -5,21 +5,23 @@ from noise import standard_noise_channels
 from pruning_circuits import generate_random_circuit
 
 
-n_qubits = 3
+n_qubits = 1
 noise = standard_noise_channels(1.889e-3, n_qubits)
 
-circuit1, key = generate_random_circuit(n_qubits, 40)
-# circuit1 = "".join(random.choices("SXHT", k=10))
-# key = ops
+# circuit1, key = generate_random_circuit(n_qubits, 40)
+circuit1 = "".join(random.choices("SXHT", k=10))
+key = ops
 
 remover = SubcircuitRemover(circuit1, key, noise, n_qubits=n_qubits)
 u1 = remover.unitary
+print(circuit1)
 print("The old circuit run with noise has this fidelity to the target unitary", f_pro_experimental(circuit1, u1, noise, key))
 
 remover.reduce_circuit()
 
 circuit2 = remover.circuit
-print("I removed", len(circuit1) - len(circuit2), "gates out of", len(circuit1))
+print(circuit2)
+print("I reduced the gate count by", len(circuit1) - len(circuit2), "out of", len(circuit1))
 u2 = remover.unitary
 print("The new circuit run with noise has this fidelity to the target unitary", f_pro_experimental(circuit2, u1, noise, key))
 
